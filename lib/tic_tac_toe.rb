b64114c21a53a0014db2d653869dc8fa8848a61e
Sean Lucius <seanlucius22@icloud.com>
2018-10-03 03:33:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,16 @@ class TicTacToe
     input_to_index.between?(0,8) && !position_taken?
   end
 
-
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
